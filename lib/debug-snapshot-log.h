f3d02662cd84c9a06e4f2969d8ffd0cc134ceeba
Stricted <info@stricted.net>
2020-04-24 17:50:57 +0000

    lib: Remove libdss
    We don't need this feature.
    
    Change-Id: I61267b660ec2570ab30911c044b478437fe43548

--- lib/debug-snapshot-log.h
+++ lib/debug-snapshot-log.h
@@ -2,8 +2,6 @@
 #ifndef DEBUG_SNAPSHOT_LOG_H
 #define DEBUG_SNAPSHOT_LOG_H
 
-#include <generated/autoconf.h>
-
 #ifdef CONFIG_DEBUG_SNAPSHOT_BINDER
 #include <linux/debug-snapshot-binder.h>
 #endif
@@ -13,11 +11,10 @@
 #define TASK_COMM_LEN 16
 #define NR_CPUS 8
 #undef CONFIG_DEBUG_SNAPSHOT_LINUX_BUILD
-#include <stdio.h>
-#include <stdlib.h>
 
 #else // DSS_ANALYZER
 
+#include <generated/autoconf.h>
 #include <linux/clk-provider.h>
 #include <linux/debug-snapshot.h>
 #include <linux/debug-snapshot-helper.h>
