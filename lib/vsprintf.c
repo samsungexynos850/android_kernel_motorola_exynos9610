b8e556a098cd6182963dd436ad7f1478ba5d51e4
Thomas Turner <tturner@lineageos.org>
2026-05-25 16:25:45 +0100

    fixup! lib/vsprintf.c: eliminate potential race in string()
    Change-Id: Ib02a00241a3136aca802fe53c3b981668390a6ac

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -612,7 +612,7 @@ static noinline_for_stack
 char *string(char *buf, char *end, const char *s, struct printf_spec spec)
 {
 	int len = 0;
-	int lim = spec.precision;
+	size_t lim = spec.precision;
 
 	if ((unsigned long)s < PAGE_SIZE)
 		s = "(null)";
